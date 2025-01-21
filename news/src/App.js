d56d1082a3078eebe807016feeae235cf9895fa4
HyeongJun Kim <khj029956@naver.com>
2025-01-21 14:08:04 +0900

    Feat: 테슬라 뉴스 정보 가져옴

--- news/src/App.js
+++ news/src/App.js
@@ -1,24 +1,7 @@
-import { useState } from 'react';
-import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get();
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
