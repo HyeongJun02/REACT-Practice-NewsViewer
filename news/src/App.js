ed9cf076047562f539874378f248e41c82b7c0b5
HyeongJun Kim <khj029956@naver.com>
2025-01-21 13:18:28 +0900

    Doc: News Viewer 앱 생성

---
+++ news/src/App.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get();
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
+    </div>
+  );
+};
+
+export default App;
